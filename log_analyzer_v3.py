9abe291bb818a6ee73673288ddd62b3a6a6d1183
Kumar Jha <deepak.kumarjha@dxc.com>
2026-06-24 11:31:03 +0530

    Added CSV report generation

---
+++ log_analyzer_v3.py
@@ -0,0 +1,29 @@
+import csv
+
+file_name = "application.log"
+
+info_count = 0
+warning_count = 0
+error_count = 0
+total_records = 0
+
+with open(file_name, "r") as file:
+    for line in file:
+        total_records += 1
+
+        if "INFO" in line:
+            info_count += 1
+        elif "WARNING" in line:
+            warning_count += 1
+        elif "ERROR" in line:
+            error_count += 1
+
+with open("log_report.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+
+    writer.writerow(["Metric", "Count"])
+    writer.writerow(["INFO", info_count])
+    writer.writerow(["WARNING", warning_count])
+    writer.writerow(["ERROR", error_count])
+
+print("CSV report generated successfully")
